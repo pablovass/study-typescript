c3e72e4627ef5c05f24e96d933c3b10cf0871511
Pablo Vallejos <faktory.fm@live.com>
2024-02-18 17:25:11 -0300

    feat : Alias y tipos literales finish

--- prototipe-typescript/src/10-alias.ts
+++ prototipe-typescript/src/10-alias.ts
@@ -1,7 +1,29 @@
 (()=>{
 // para crear los alias tenemos que crear tipo de objetos que los vamos a llamar types
 type UserId =string|number;
+let UserId:UserId;
 
+function greeting(UserId:UserId){
+  if(typeof UserId === 'string'){
+    console.log(`string ${UserId.toLowerCase()}`)
+  }
+}
 
+//literal types
+type Sizes = 'S'|'M'|'L'|'XL';
+let shirtSize : Sizes;
+shirtSize ='L';
+shirtSize ='M';
+shirtSize ='S';
+shirtSize ='XL';
+
+function greeting2(UserId:UserId, sizes:Sizes){
+  if(typeof UserId === 'string'){
+    console.log(`string ${UserId.toLowerCase()}`)
+  }
+}
+
+greeting2(111 , 'L');
+greeting2('111' , 'L');
 
 })();
