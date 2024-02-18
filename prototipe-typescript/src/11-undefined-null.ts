6d4d8b9c33dd6cc4f60b24f9d0671924c56ca21b
Pablo Vallejos <faktory.fm@live.com>
2024-02-18 17:42:48 -0300

    feat: Null y Undefined

---
+++ prototipe-typescript/src/11-undefined-null.ts
@@ -0,0 +1,19 @@
+(() => {
+  // las variables a continuacion no se puede declarar
+  //let mynumber:number=undefined;
+  //let myString:string=null
+
+  let myNull: null = null;
+  let MyUndefined: undefined = undefined;
+
+  let mynumber: number | null = null;
+  let myString: string | undefined = undefined;
+  myString='fsf0';
+
+  function hi(name: string | null ){
+    let hello ='hola' ;
+  if(name){
+    hello+= ''
+  }
+  }
+})();
