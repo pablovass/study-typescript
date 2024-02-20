d70b6e4b4cfe39693babc20e551772bd805d8cd5
Pablo Vallejos <faktory.fm@live.com>
2024-02-20 13:09:04 -0300

    Usando librerías que NO soportan TypeScript

---
+++ prototipe-typescript/src/17-load.ts
@@ -0,0 +1,28 @@
+//var _ = requiere('');   esta no soporta tipos
+
+import _ from 'lodash'; // el mismo  editor visual studio code te dice, el comando que tenes que utilizar para crearle tipos a un libreria que no tiene tipos
+
+const data =[
+  {
+    username:'nico',
+    role:'admin'
+  },
+  {
+    username:'valentin',
+    role:'seller'
+  },
+  {
+    username:'mariano',
+    role:'seller'
+  },
+  {
+    username:'pablo',
+    role:'cutomer'
+  }
+];
+const rta= _.groupBy(data,(item)=>item.role);
+
+console.log(rta);
+
+
+
